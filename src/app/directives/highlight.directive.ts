772042ffbd63354d9788e26c99c2490e1fad8af3
Geraldine Nitaly Meneses <geraldine.nitaly@gmail.com>
2023-01-05 12:37:11 -0500

    feat(directives): creacion y manipulacion de directivas para estilo

---
+++ src/app/directives/highlight.directive.ts
@@ -0,0 +1,20 @@
+import { Directive, ElementRef, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  @HostListener('mouseenter') onMouseEnter(){
+    this.element.nativeElement.style.backgroundColor = '#D7BDE2';
+  }
+  @HostListener('mouseleave') onMouseLeave(){
+    this.element.nativeElement.style.backgroundColor = '';
+  }
+  constructor(
+    private element: ElementRef
+  ) {
+    //this.element.nativeElement.style.backgroundColor = '#D7BDE2';
+  }
+
+}
